1331ce8ffae5e01569697da83a81b89c6462df97
AlexGerzen <121639829+AlexGerzen@users.noreply.github.com>
2023-11-17 16:34:19 +0100

    Finished Add Ticket

--- src/app/add-ticket/add-ticket.component.ts
+++ src/app/add-ticket/add-ticket.component.ts
@@ -1,5 +1,7 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
 import { FormControl, Validators } from '@angular/forms';
+import { Firestore, collection, addDoc } from '@angular/fire/firestore';
+import { Ticket } from 'src/models/ticket.class';
 
 @Component({
   selector: 'app-add-ticket',
@@ -10,6 +12,44 @@ export class AddTicketComponent {
   email = new FormControl('', [Validators.required, Validators.email]);
   titel = new FormControl('', [Validators.required]);
   urgency = new FormControl('', [Validators.required]);
+  description = new FormControl('', [Validators.required]);
+  company = new FormControl('', [Validators.required]);
+
+  ticket = new Ticket();
+  loading: boolean = false;
+  dueDate: Date;
+
+  private firestore: Firestore = inject(Firestore);
+
+  constructor() {
+    
+  }
+
+
+
+  async safeTicket() {
+    this.loading = true;
+    this.ticket.dueDate = this.dueDate.getTime();
+    this.ticket.date = new Date();
+    this.ticket.ticketNumber = this.createTicketnumber();
+    this.ticket.ticketStatus = "Requested"
+    await this.addDocument();
+    this.loading = false;
+  }
+
+  createTicketnumber() {
+    let timestamp = new Date().getTime();
+    var random = Math.floor(Math.random() * (9999 - 1000 + 1)) + 1000;
+    var ticketNumber = timestamp + '' + random;
+    ticketNumber = ticketNumber.slice(0, 10);
+    console.log(ticketNumber);
+    
+    return ticketNumber;
+  }
+
+  async addDocument() {
+    await addDoc(collection(this.firestore, 'tickets'), this.ticket.toJson())
+  }
 
   getErrorMessageEmail() {
     if (this.email.hasError('required')) {
--- src/models/ticket.class.ts
+++ src/models/ticket.class.ts
@@ -1,34 +1,40 @@
 export class Ticket {
     title: string;
-    date: number;
-    importance: string;
+    date: Date;
+    dueDate: number;
+    urgency: string;
     assignedEmployee: string;
-    ticketNumber: number;
+    ticketNumber: string;
     ticketStatus: string;
     contact: string;
     description: string;
+    company:string;
 
     constructor(obj?: any) {
         this.title = obj ? obj.title : '';
         this.date = obj ? obj.date : '';
-        this.importance = obj ? obj.importance : '';
+        this.dueDate = obj ? obj.dueDate : '';
+        this.urgency = obj ? obj.importance : '';
         this.assignedEmployee = obj ? obj.assignedEmployee : '';
         this.ticketNumber = obj ? obj.ticketNumber : '';
         this.ticketStatus = obj ? obj.ticketStatus : '';
         this.contact = obj ? obj.contact : '';
         this.description = obj ? obj.description : '';
+        this.company = obj ? obj.company : '';
     }
 
     public toJson() {
         return {
             title: this.title,
             date: this.date,
-            importance: this.importance,
+            urgency: this.urgency,
             assignedEmployee: this.assignedEmployee,
             ticketNumber: this.ticketNumber,
             ticketStatus: this.ticketStatus,
             contact: this.contact,
             description: this.description,
+            dueDate: this.dueDate,
+            company : this.company,
         }
     }
 }
